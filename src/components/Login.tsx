72b4b0b75213a97dc66d53e54ad4613eb7d655a6
Edgar Santos <edgarrps@gmail.com>
2023-02-14 00:52:25 -0400

    primitive version of the login screen

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router-dom"
+import LoginImg from '../img/Login.svg'
 
 export const Login = () => {
   return (
     <div>
-      <label>Login</label>
-      <div>
-        <input type='text' placeholder="usuário"></input>
-        <input type='password' placeholder="senha"></input>
+      <div className='justify-content'>
+        <img className='max-w-[400px] mx-auto pb-10' src={LoginImg} />
       </div>
-      <div>
-        <Link to='./tickets'><button type='submit'>Login</button></Link>
-      </div>
+
+
+      <form className='bg-white space-y-2 max-w-[300px] w-full mx-auto rounded-2xl bg-gray-400 p-8 px-8'>
+
+        <div className='text-center pb-1'>
+          <label className='text-3xl text-gray-600 font-bold'>Entrar</label>
+        </div>
+
+        <div>
+          <div><input className='placeholder:text-center bg-gray-100 border-2 outline-yellow-500 border-yellow-300 text-gray-600 text-sm rounded-full block w-full p-1.5' type='text' placeholder="usuário"></input></div>
+          <div className='pt-4 pb-4'><input className='placeholder:text-center bg-gray-100 border-2 outline-yellow-500 border-yellow-300 text-gray-600 text-sm rounded-full block w-full p-1.5' type='password' placeholder="senha"></input></div>
+        </div>
+
+        <div className='grid place-items-center'>
+          <Link to='./tickets'><button className='text-white bg-yellow-600 hover:bg-yellow-500 focus:ring-4 focus:outline-none font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center' type='submit'>Login</button></Link>
+        </div>
+
+      </form>
+
     </div>
   )
 }
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,26 +1,37 @@
+import JoinUs from '../img/JoinUs.svg'
 
 export const Register = () => {
     return (
-        <div><label>Usuário novo? Registrar</label>
-            <div>
-                <label htmlFor='username'>Nome do Usuário</label>
-                <input id='username' type='text' placeholder='username'></input>
+        <div>
+
+            <div className='justify-content'>
+                <img className='max-w-[230px] mx-auto pt-10 pb-10' src={JoinUs} />
             </div>
-            <div>
-                <label htmlFor='email'>E-mail</label>
-                <input id='email' type='email' placeholder='user@user.com'></input>
-            </div>
-            <div>
-                <label htmlFor='passwd'>Senha</label>
-                <input id='passwd' type='password' placeholder='********'></input>
-            </div>
-            <div>
-                <label htmlFor='repasswd'>Repetir senha</label>
-                <input id='repasswd' type='password' placeholder='********'></input>
-            </div>
-            <div>
-                <button type='submit'>Registrar</button>
-            </div>
+            <form className='bg-white space-y-2 max-w-[300px] w-full mx-auto rounded-2xl bg-gray-400 p-8 px-8'>
+
+                <div className='text-center pb-1'>
+                    <label className='text-3xl text-gray-600 font-bold'>Usuário novo?</label><label className='text-2xl text-gray-600 font-bold'>Registre-se</label>
+                </div>
+                <div className='text-center'>
+                    <label htmlFor='username'>Nome do Usuário</label>
+                    <input className='placeholder:text-center bg-gray-100 border-2 outline-yellow-500 border-yellow-300 text-gray-600 text-sm rounded-full block w-full p-1.5' id='username' type='text' placeholder='username'></input>
+                </div>
+                <div className='text-center'>
+                    <label htmlFor='email'>E-mail</label>
+                    <input className='placeholder:text-center bg-gray-100 border-2 outline-yellow-500 border-yellow-300 text-gray-600 text-sm rounded-full block w-full p-1.5' id='email' type='email' placeholder='user@user.com'></input>
+                </div>
+                <div className='text-center'>
+                    <label htmlFor='passwd'>Senha</label>
+                    <input className='placeholder:text-center bg-gray-100 border-2 outline-yellow-500 border-yellow-300 text-gray-600 text-sm rounded-full block w-full p-1.5' id='passwd' type='password' placeholder='********'></input>
+                </div>
+                <div className='text-center'>
+                    <label htmlFor='repasswd'>Repetir senha</label>
+                    <input className='placeholder:text-center bg-gray-100 border-2 outline-yellow-500 border-yellow-300 text-gray-600 text-sm rounded-full block w-full p-1.5' id='repasswd' type='password' placeholder='********'></input>
+                </div>
+                <div className='text-center'>
+                    <button className='text-white bg-yellow-600 hover:bg-yellow-500 focus:ring-4 focus:outline-none font-medium rounded-full text-sm w-full sm:w-auto px-5 py-2.5 text-center' type='submit'>Registrar</button>
+                </div>
+            </form>
         </div>
     )
 }
