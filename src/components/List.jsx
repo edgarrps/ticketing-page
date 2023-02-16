1585f6f46c89184bdefc48be775cb4af83d151e0
Edgar Santos <edgarrps@gmail.com>
2023-02-16 19:43:38 -0400

    style in list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,36 +1,37 @@
-import { Link } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 export const List = () => {
-  const [tickets, setTickets] = useState([])
+  const [tickets, setTickets] = useState([]);
 
   useEffect(() => {
-    const storedTickets = JSON.parse(localStorage.getItem('Tickets')) || []
-    setTickets(storedTickets)
-  }, [])
+    const storedTickets = JSON.parse(localStorage.getItem('Tickets')) || [];
+    setTickets(storedTickets);
+  }, []);
 
   return (
     <div>
-      <h2>Lista de Tickets</h2>
-      <table>
+      <h2 className='px-6 py-3 text-left  uppercase tracking-wider'>Lista de Tickets</h2>
+      <table className='w-full divide-y divide-gray-200'>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Título</th>
-            <th>Categoria</th>
-            <th>Urgência</th>
-            <th>Usuário</th>
+            <th className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>ID</th>
+            <th className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>Título</th>
+            <th className='px-6 py-3 text-left  text-xs font-4xl text-grady-500 uppercase tracking-wider'>Usuário</th>
+            <th className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>Categoria</th>
+            <th className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>Urgência</th>
           </tr>
         </thead>
         <tbody>
           {tickets.map((ticket) => (
             <tr key={ticket.id}>
               <td>
-                <Link to={`/tickets/${ticket.id}`}>{ticket.id}</Link>
+                <Link className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider' to={`/tickets/${ticket.id}`}>{ticket.id}</Link>
               </td>
-              <td>{ticket.title}</td>
-              <td>{ticket.category}</td>
-              <td>{ticket.urgency}</td>
-              <td>{ticket.user}</td>
+              <td className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>{ticket.title}</td>
+              <td className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>{ticket.user}</td>
+              <td className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>{ticket.category}</td>
+              <td className='px-6 py-3 text-left text-xs font-4xl text-grady-500 uppercase tracking-wider'>{ticket.urgency}</td>
             </tr>
           ))}
         </tbody>
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,32 +1,28 @@
-import { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Tickets } from './Tickets'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export const Details = () => {
   const { id } = useParams()
   const [ticket, setTicket] = useState(null)
-  
+
   useEffect(() => {
     const storedTickets = JSON.parse(localStorage.getItem('Tickets')) || []
-    const selectedTicket = storedTickets.find((ticket) => ticket.id === parseInt(id))
-    setTicket(selectedTicket)
-    console.log({Tickets})
+    const foundTicket = storedTickets.find((ticket) => ticket.id === parseInt(id))
+    setTicket(foundTicket)
   }, [id])
 
-  
+    if (!ticket) {
+      return <p>Ticket não encontrado.</p>
+    }
+
   return (
     <div>
-      {ticket ? (
-        <>
-          <h1>{ticket.title}</h1>
-          <p>{ticket.description}</p>
-          <form>
-          </form>
-          <Link to='/tickets'><button>Voltar</button></Link>
-        </>
-      ) : (
-        <p>Ticket não encontrado</p>
-      )}
+      <h2>Detalhes do Ticket {ticket.id}</h2>
+      <p><strong>Título:</strong> {ticket.title}</p>
+      <p><strong>Categoria:</strong> {ticket.category}</p>
+      <p><strong>Urgência:</strong> {ticket.urgency}</p>
+      <p><strong>Descrição:</strong> {ticket.description}</p>
+      <p><strong>Usuário:</strong> {ticket.user}</p>
     </div>
   )
 }
